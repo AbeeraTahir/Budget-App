6eb5096dd3798bf69b6b3d55d3dc4ba459b894ab
Abeera Tahir <abeeratahir2018@gmail.com>
2023-03-21 00:09:44 +0500

    Add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :groups, class_name: 'Group', dependent: :destroy, foreign_key: 'user_id'
   has_many :expenses, class_name: 'Expense', dependent: :destroy, foreign_key: 'user_id'
 
