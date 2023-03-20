2ed978b43c411a437ef06f99aadbd7dc0ee894c7
Abeera Tahir <abeeratahir2018@gmail.com>
2023-03-21 01:45:59 +0500

    Add new category page

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,2 @@
+class ExpensesController < ApplicationController
+end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,17 @@
 class GroupsController < ApplicationController
+  before_action :set_user
+
   def index
+    @groups = @user.groups.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  private
+
+  def set_user
+    @user = current_user
   end
 end
