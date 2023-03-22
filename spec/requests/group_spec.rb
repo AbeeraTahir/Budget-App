b81a7188d432316ab4b069e380f8704fbfbb6430
Abeera Tahir <abeeratahir2018@gmail.com>
2023-03-22 23:06:37 +0500

    Update integration specs for groups

--- spec/requests/group_spec.rb
+++ spec/requests/group_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Groups page', type: :system do
   describe 'Groups /index' do
     before(:each) do
-      @user = User.create!(name: 'name', email: 'email@gmail.com', password: 'password')
+      @user = User.create!(name: 'name', email: 'email@gmail.com', password: '123456')
       @icon_file = fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'test.png'), 'image/png')
       @group = Group.create(name: 'food', icon: @icon_file, user: @user)
       @user.skip_confirmation!
       @user.save!
       visit new_user_session_path
       fill_in 'user_email', with: 'email@gmail.com'
-      fill_in 'user_password', with: 'password'
+      fill_in 'user_password', with: '123456'
       click_button 'Log in'
       sleep(1)
       visit groups_path
@@ -20,7 +20,7 @@ RSpec.describe 'Groups page', type: :system do
       sleep(2)
     end
 
-    it 'displays the name, date, icon, amount of group' do
+    it 'displays the name, date, amount of group' do
       expect(page).to have_content(@group.name)
       expect(page).to have_content(@group.created_at.strftime("%d-%m-%Y"))
       expect(page).to have_content(@group.expenses.sum(:amount))
@@ -38,6 +38,39 @@ RSpec.describe 'Groups page', type: :system do
       link.click
       sleep(1)
       expect(page).to have_current_path new_group_path
+    end
+  end
+  describe 'Groups /show' do
+    before(:each) do
+      @user = User.create!(name: 'name', email: 'email@gmail.com', password: '123456')
+      @icon_file = fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'test.png'), 'image/png')
+      @group = Group.create(name: 'food', icon: @icon_file, user: @user)
+      @expense = Expense.create(name: 'cake', amount: 100, group_ids: [@group.id], user: @user)
+      @user.skip_confirmation!
+      @user.save!
+      visit new_user_session_path
+      fill_in 'user_email', with: 'email@gmail.com'
+      fill_in 'user_password', with: '123456'
+      click_button 'Log in'
+      sleep(1)
+      visit group_path(@group)
+    end
+
+    after(:each) do
+      sleep(2)
+    end
+
+    it 'displays the name, date, amount of expense' do
+      expect(page).to have_content(@expense.name)
+      expect(page).to have_content(@expense.created_at.strftime("%d-%m-%Y"))
+      expect(page).to have_content(@expense.amount)
+    end
+
+    it 'click on add new transaction button' do
+      link = find('a', text: 'Add new transaction', match: :first)
+      link.click
+      sleep(1)
+      expect(page).to have_current_path new_group_expense_path(@group)
     end
   end
 end
