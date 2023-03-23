b71508ea0dfa302f5ec1c21c7e217e51893dad2c
Abeera Tahir <abeeratahir2018@gmail.com>
2023-03-24 03:35:42 +0500

    Complete styling

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,22 +1,28 @@
 class ExpensesController < ApplicationController
+  before_action :find_group
+  
   def new
     @expense = Expense.new
   end
 
   def create
-    @group = Group.find(params[:group_id])
     unless @group.user == current_user
       return redirect_to groups_path, notice: 'You can only create expenses from your categories'
     end
-    @expense = Expense.new(expense_params)
-    @expense.user = current_user
-
-    if @expense.save
-      flash[:notice] = 'Expense created successfully'
-      redirect_to @group
+    
+    if expense_params[:group_ids].length == 1
+      return redirect_to new_group_expense_path(@group), alert: 'Must select at least one category'
     else
-      flash[:alert] = @expense.errors.full_messages.first if @expense.errors.any?
-      render :new, status: :unprocessable_entity
+      @expense = Expense.new(expense_params)
+      @expense.user = current_user
+
+      if @expense.save
+        flash[:notice] = 'Expense created successfully'
+        redirect_to @group
+      else
+        flash[:alert] = @expense.errors.full_messages.first if @expense.errors.any?
+        render :new, status: :unprocessable_entity
+      end
     end
   end
 
@@ -24,5 +30,9 @@ class ExpensesController < ApplicationController
 
   def expense_params
     params.require(:expense).permit(:name, :amount, group_ids: [])
+  end
+
+  def find_group
+    @group = Group.find(params[:group_id])
   end
 end
